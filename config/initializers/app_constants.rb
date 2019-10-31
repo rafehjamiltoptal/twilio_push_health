916316c7694686fc7a22d325f7059ce727e17d4c
rafehdevbasement <rafeh.jamil@devbasement.com>
2019-11-01 02:48:17 +0500

    removed tiwlio keys

--- config/initializers/app_constants.rb
+++ config/initializers/app_constants.rb
@@ -1,18 +1,2 @@
 APP_NAME = 'Twilio Project'.freeze
-TWILIO_ACCOUNT_SID = if Rails.env.production?
-                       'ACf559f7f04e38c3fa3ea80c713763d469'.freeze
-                     elsif Rails.env.staging?
-                       'ACf559f7f04e38c3fa3ea80c713763d469'.freeze
-                     else
-                       'ACf559f7f04e38c3fa3ea80c713763d469'.freeze
-                     end
-
-TWILIO_AUTH_TOKEN = if Rails.env.production?
-                      'f8f4a1084eb7f3a8891a62c7ce78f489'.freeze
-                    elsif Rails.env.staging?
-                      'f8f4a1084eb7f3a8891a62c7ce78f489'.freeze
-                    else
-                      'f8f4a1084eb7f3a8891a62c7ce78f489'.freeze
-                    end
-
 DEFAULT_VERIFICATION_CODE_LENGTH = 6
