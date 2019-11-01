63c0979da261a9e6414e2c4ca9f0d3beab125ed5
rafehdevbasement <rafeh.jamil@devbasement.com>
2019-11-02 02:05:59 +0500

    environment set up

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module TwilioProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.active_job.queue_adapter = :sidekiq
+    # config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
