dc5df362acd139b3653f8b7bd1a96502ce60aa56
rafehjamil <rafeh.jamil@toptal.com>
2019-11-13 23:13:54 +0500

    optimized twilio api

--- lib/twilio_api.rb
+++ lib/twilio_api.rb
@@ -20,6 +20,8 @@ module TwilioAPI
         'To'   => number_from,
         'Url'  => call_back_url(number_to)
       }
+      # for testing
+      # {"answered_by": null, "price_unit": "USD", "parent_call_sid": null, "caller_name": null, "group_sid": null, "duration": null, "from": "+12052368178", "to": "+923015182390", "annotation": null, "date_updated": null, "sid": "CAa8a44a42dd7c07f8b629e12bc16ee46d", "price": null, "api_version": "2010-04-01", "status": "queued", "direction": "outbound-api", "start_time": null, "date_created": null, "from_formatted": "(205) 236-8178", "forwarded_from": null, "uri": "/2010-04-01/Accounts/ACf559f7f04e38c3fa3ea80c713763d469/Calls/CAa8a44a42dd7c07f8b629e12bc16ee46d.json", "account_sid": "ACf559f7f04e38c3fa3ea80c713763d469", "end_time": null, "to_formatted": "+923015182390", "phone_number_sid": "PNb46a5c5da73a699bb038509074c891cf", "subresource_uris": {"notifications": "/2010-04-01/Accounts/ACf559f7f04e38c3fa3ea80c713763d469/Calls/CAa8a44a42dd7c07f8b629e12bc16ee46d/Notifications.json", "recordings": "/2010-04-01/Accounts/ACf559f7f04e38c3fa3ea80c713763d469/Calls/CAa8a44a42dd7c07f8b629e12bc16ee46d/Recordings.json", "feedback": "/2010-04-01/Accounts/ACf559f7f04e38c3fa3ea80c713763d469/Calls/CAa8a44a42dd7c07f8b629e12bc16ee46d/Feedback.json", "feedback_summaries": "/2010-04-01/Accounts/ACf559f7f04e38c3fa3ea80c713763d469/Calls/FeedbackSummary.json"}}
       twilio_api(:post, 'Calls', params)
     end
 
@@ -29,6 +31,8 @@ module TwilioAPI
         'To'   => number,
         'Body' => message_body
       }
+      # for testing
+      # {"sid": "SM9905a2f7f8c840dea4dfe7d71e8714b1", "date_created": "Tue, 12 Nov 2019 19:21:04 +0000", "date_updated": "Tue, 12 Nov 2019 19:21:04 +0000", "date_sent": null, "account_sid": "ACf559f7f04e38c3fa3ea80c713763d469", "to": "+923015182390", "from": "+12052368178", "messaging_service_sid": null, "body": "Sent from your Twilio trial account - 123123", "status": "queued", "num_segments": "1", "num_media": "0", "direction": "outbound-api", "api_version": "2010-04-01", "price": null, "price_unit": "USD", "error_code": null, "error_message": null, "uri": "/2010-04-01/Accounts/ACf559f7f04e38c3fa3ea80c713763d469/Messages/SM9905a2f7f8c840dea4dfe7d71e8714b1.json", "subresource_uris": {"media": "/2010-04-01/Accounts/ACf559f7f04e38c3fa3ea80c713763d469/Messages/SM9905a2f7f8c840dea4dfe7d71e8714b1/Media.json"}}
       twilio_api(:post, 'Messages', params)
     end
 
@@ -72,7 +76,7 @@ module TwilioAPI
       "#{ENV['APP_HOST']}/twilio/connect?number_to=#{number_to}"
     end
 
-    # Following functions has been copied from Twilio Gem security/request_validator.rb file
+    # Following functions have been copied from Twilio Gem security/request_validator.rb file
     ##
     # Validates that after hashing a request with Twilio's request-signing algorithm
     # (https://www.twilio.com/docs/usage/security#validating-requests), the hash matches the signature parameter
@@ -85,16 +89,9 @@ module TwilioAPI
     #
     # @return [Boolean] whether or not the computed signature matches the signature parameter
     def validate(url, params, signature)
-      params_hash = body_or_hash(params)
-      if params_hash.is_a? Enumerable
-        expected = build_signature_for(url, params_hash)
-        secure_compare(expected, signature)
-      else
-        expected_signature = build_signature_for(url, {})
-        body_hash = URI.decode_www_form(URI(url).query).to_h['bodySHA256']
-        expected_hash = build_hash_for(params)
-        secure_compare(expected_signature, signature) && secure_compare(expected_hash, body_hash)
-      end
+      params_hash = params.to_unsafe_h
+      expected = build_signature_for(url, params_hash)
+      ActiveSupport::SecurityUtils.secure_compare(expected, signature)
     end
 
     ##
@@ -108,47 +105,6 @@ module TwilioAPI
       data = url + params.sort.join
       digest = OpenSSL::Digest.new('sha1')
       Base64.strict_encode64(OpenSSL::HMAC.digest(digest, ENV['TWILIO_AUTH_TOKEN'], data))
-    end
-
-    ##
-    # Build a SHA256 hash for a body string
-    #
-    # @param [String] body String to hash
-    #
-    # @return [String] A hex-encoded SHA256 of the body string
-    def build_hash_for(body)
-      hasher = OpenSSL::Digest.new('sha256')
-      hasher.hexdigest(body)
-    end
-
-    # Compares two strings in constant time to avoid timing attacks.
-    # Borrowed from ActiveSupport::MessageVerifier.
-    # https://github.com/rails/rails/blob/master/activesupport/lib/active_support/message_verifier.rb
-    def secure_compare(a, b)
-      return false unless a.bytesize == b.bytesize
-
-      l = a.unpack("C#{a.bytesize}")
-
-      res = 0
-      b.each_byte { |byte| res |= byte ^ l.shift }
-      res.zero?
-    end
-
-    # `ActionController::Parameters` no longer, as of Rails 5, inherits
-    # from `Hash` so the `sort` method, used above in `build_signature_for`
-    # is deprecated.
-    #
-    # `to_unsafe_h` was introduced in Rails 4.2.1, before then it is still
-    # possible to sort on an ActionController::Parameters object.
-    #
-    # We use `to_unsafe_h` as `to_h` returns a hash of the permitted
-    # parameters only and we need all the parameters to create the signature.
-    def body_or_hash(params_or_body)
-      if params_or_body.respond_to?(:to_unsafe_h)
-        params_or_body.to_unsafe_h
-      else
-        params_or_body
-      end
     end
   end
 end
